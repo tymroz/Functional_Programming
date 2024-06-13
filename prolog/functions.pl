daba5949b669dc7d546a7bd3ee4d3da4664f57c4
Tymoteusz <tymoteusz.rozmiarek89@gmail.com>
2024-06-13 16:53:29 +0200

    poprawki prolog

--- prolog/functions.pl
+++ prolog/functions.pl
@@ -71,9 +71,18 @@ primes(N, Primes) :-
     findall(X, (between(2, N, X), is_prime(X)), Primes).
 
 is_prime(2).
+is_prime(3).
 is_prime(N) :-
-    N > 2,
-    \+ (between(2, M, F), M is floor(sqrt(N)), N mod F =:= 0).
+    N > 3,
+    N mod 2 =\= 0,
+    \+ has_factor(N, 3).
+
+has_factor(N, F) :-
+    N mod F =:= 0.
+has_factor(N, F) :-
+    F * F < N,
+    F2 is F + 2,
+    has_factor(N, F2).
 
 between(Low, High, Low) :-
     Low =< High.
